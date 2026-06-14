773bf9ed8fc4cb1c77aaee5d32d4be8d4c2c196f
Akshat-Datt <dattakshat31@gmail.com>
2026-06-14 17:50:30 +0530

    redesigning result activity with score/total questions, some optimizations, accuracy, a motivating message and play again CTA

--- app/src/main/java/com/unit/triviaapp/activity/ResultActivity.kt
+++ app/src/main/java/com/unit/triviaapp/activity/ResultActivity.kt
@@ -1,11 +1,14 @@
 package com.unit.triviaapp.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.unit.triviaapp.R
 import com.unit.triviaapp.constants.ConstKeys
+import java.util.Locale
 
 class ResultActivity: AppCompatActivity() {
 
@@ -15,16 +18,42 @@ class ResultActivity: AppCompatActivity() {
         setContentView(R.layout.activity_result)
 
         val score = findViewById<TextView>(R.id.tvScore)
-        val totalQuestions = findViewById<TextView>(R.id.tvTotalQuestions)
         val accuracy = findViewById<TextView>(R.id.tvAccuracy)
+        val performance = findViewById<TextView>(R.id.tvPerformance)
+        val btnPlayAgain = findViewById<Button>(R.id.btnPlayAgain)
 
         val scoreFetched = intent.getIntExtra(ConstKeys.SCORE, 0)
         val totalQuestionsFetched = intent.getIntExtra(ConstKeys.TOTAL_QUESTIONS, 0)
         val accuracyFetched = intent.getFloatExtra(ConstKeys.ACCURACY, 0.0f)
 
-        score.text = scoreFetched.toString()
-        totalQuestions.text = totalQuestionsFetched.toString()
-        accuracy.text = accuracyFetched.toString()
+        score.text = getString(
+            R.string.score,
+            scoreFetched,
+            totalQuestionsFetched
+        )
+
+        val roundOffAccuracy = String.format(Locale.getDefault(), "%.2f%%", accuracyFetched)
+        accuracy.text = getString(
+            R.string.accuracy,
+            roundOffAccuracy
+        )
+
+        val performanceMessage = when {
+            accuracyFetched == 100f -> "Perfect Score! 🎯"
+            accuracyFetched >= 90f -> "Quiz Master! 🧠"
+            accuracyFetched >= 70f -> "Great Job! 🔥"
+            accuracyFetched >= 50f -> "Good Attempt 👍"
+            else -> "Keep Practicing 💪"
+        }
+
+        performance.text = performanceMessage
+
+        btnPlayAgain.setOnClickListener {
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+            finish()
+        }
 
     }
 }
