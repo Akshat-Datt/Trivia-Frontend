3431e8b09712d3ba6750fb20ef8896cc4bcb6095
Akshat-Datt <dattakshat31@gmail.com>
2026-07-29 21:54:27 +0530

    creating parcable model of endless quiz reponse and linked it to endpoint call

--- app/src/main/java/com/unit/triviaapp/network/ApiService.kt
+++ app/src/main/java/com/unit/triviaapp/network/ApiService.kt
@@ -1,5 +1,6 @@
 package com.unit.triviaapp.network
 
+import com.unit.triviaapp.models.EndlessQuestionResponse
 import com.unit.triviaapp.models.Platforms
 import com.unit.triviaapp.models.Question
 import com.unit.triviaapp.models.QuizResultResponse
@@ -8,6 +9,7 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ApiService {
 
@@ -19,4 +21,11 @@ interface ApiService {
 
     @POST("submit/questions")
     fun submitQuestions(@Body request: SubmitQuizRequest): Call<QuizResultResponse>
+
+    @GET("questions/endless-quiz")
+    fun getEndlessQuiz(
+        @Query("platform_id") platformId: Int,
+        @Query("page") page: Int?,
+        @Query("limit") limit: Int?
+    ): Call<EndlessQuestionResponse>
 }
