e523013caf3134e548f89ed3b5f4652c2f4acaf5
Akshat-Datt <dattakshat31@gmail.com>
2026-09-06 11:45:08 +0530

    introducing a row in home screen composable

--- app/src/main/java/com/unit/triviaapp/constants/ConstKeys.kt
+++ app/src/main/java/com/unit/triviaapp/constants/ConstKeys.kt
@@ -2,8 +2,5 @@ package com.unit.triviaapp.constants
 
 object ConstKeys {
     const val QUIZ_CONFIG = "quizConfig"
-    const val SCORE = "score"
-    const val TOTAL_QUESTIONS = "totalQuestions"
-    const val ACCURACY = "accuracy"
     const val ANSWERS = ""
 }
--- app/src/main/java/com/unit/triviaapp/ui/HomeScreen.kt
+++ app/src/main/java/com/unit/triviaapp/ui/HomeScreen.kt
@@ -9,8 +9,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Text
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.ui.res.painterResource
 import com.unit.triviaapp.R
@@ -42,5 +46,18 @@ fun HomeScreen(){
         )
 
         Text("Test your knowledge against the clock")
+
+        Spacer(
+            modifier = Modifier.height(60.dp)
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(32.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text("⚡ Timed Questions")
+            Text("🏆 Challenge Yourself")
+        }
     }
 }
